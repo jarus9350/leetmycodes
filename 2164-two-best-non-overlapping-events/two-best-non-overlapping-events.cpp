f78212d8ba7f3d77ba1c7d873748231eea82a8a8
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2024-12-08 17:05:35 -0500

    Time: 2828 ms (5.17%) | Memory: 228.3 MB (5.54%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        //to make intervals inclusive
+        // for (int i = 0; i < events.size() ; i++){
+        //     events[i][1]++;
+        // }
+
+        vector<vector<int>> intervals;
+
+        for (auto e: events){
+            intervals.push_back({e[0],0,e[2]});
+            intervals.push_back({e[1],1,e[2]});
+        }
+
+        sort(intervals.begin(),intervals.end());
+
+        // for (auto i : intervals){
+        //     cout<<i[0]<<" "<<i[1]<<" "<<i[2]<<endl;
+        // }
+
+        int prevMax = 0;
+        int possibleAns = 0;
+
+        for (auto i: intervals){
+            int time = i[0];
+            int startingEnding = i[1];
+            int value = i[2];
+
+            if (startingEnding == 1){
+                prevMax = max(value,prevMax);
+            } else {
+                possibleAns = max(possibleAns,prevMax + value);
+            }
+
+            cout<<prevMax<<" "<<possibleAns<<endl;
+
+        }
+
+        return possibleAns;
+    }
+
+    // Theory
+    // https://www.youtube.com/watch?v=gdBsvEOeYho
+};
