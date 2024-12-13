38a6f96ca9a2e21a0fe4070c400179a7299ea229
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2024-12-12 20:26:49 -0500

    Time: 450 ms (13.00%) | Memory: 153.3 MB (11.76%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    struct compare{
+        bool operator()(const pair<int,int> &p1,const pair<int,int> & p2) {
+            if (p1.first == p2.first){
+                return p1.second > p2.second;
+            }
+            return p1.first > p2.first;
+        }
+    };
+
+    long long findScore(vector<int>& nums) {
+        int s = nums.size();
+        vector<pair<int,int>> ni;
+        set<int> indices;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, compare> pq;
+        for (int i = 0 ; i <  nums.size() ; i++){
+            // ni.push_back({nums[i],i});
+            indices.insert(i);
+            pq.push({nums[i],i});
+        }
+
+
+
+        // sort(ni.begin(),ni.end(),compare());
+
+        // for (auto n: ni){
+        //     cout<<n.first<<" "<<n.second<<endl;
+        // }
+
+        long long score = 0;
+
+        while (indices.size()){
+            auto top = pq.top();
+            pq.pop();
+
+            int num = top.first;
+            int index = top.second;
+
+            // cout<<num<<" "<<index<<endl;
+
+            if (indices.find(index) == indices.end()){
+                continue;
+            }
+
+            score += num;
+            indices.erase(index);
+
+            if (index - 1 >=0 ){
+                indices.erase(index-1);
+            }
+
+            if (index + 1 < s){
+                indices.erase(index+1);
+            }
+
+            // cout<<score<<endl;
+
+        }
+
+        return score;
+    }
+};
