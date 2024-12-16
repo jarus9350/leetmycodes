ba1e00bdb557d8e0d3ce7d7b20f5eda9890a7781
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2024-12-15 22:13:56 -0500

    Time: 0 ms (100.00%) | Memory: 27.7 MB (29.75%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -6,14 +6,16 @@ struct Compare {
         return a.first > b.first;
     }
 };
+
 class Solution {
 public:
-    bool compare(pair<int,int> a, pair<int,int> b){
-        if (a.first == b.first){
-            return a.second > b.second;
-        }
-        return a.first > b.first;
-    }
+    // bool compare(const pair<int,int>& a, const pair<int,int>& b){
+    //     if (a.first == b.first){
+    //         return a.second > b.second;
+    //     }
+    //     return a.first > b.first;
+    // }
+
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int len = nums.size();
         vector<int> finalState(len);
