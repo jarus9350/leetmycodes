b5d91ee716917594cd9fd9adf2463c7f9a75ed74
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2024-12-31 05:07:43 -0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (65.58%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> memo;
+    int dfs(int index, vector<int>& days, vector<int>& costs){
+        if (index == days.size()) return 0;
+
+        if (memo[index] != 0) return memo[index];
+
+        // 1 day
+        int j = index;
+        int destNum = days[index] + 1;
+        while ( j < days.size() && destNum > days[j] ) {
+            j++;
+        }
+
+        int a = costs[0] + dfs(j,days,costs);
+
+        // 1 week
+        j = index;
+        destNum = days[index] + 7;
+        while ( j < days.size() && destNum > days[j] ) {
+            j++;
+        }
+
+        int b = costs[1] + dfs(j,days,costs);
+
+        // 1 month
+        j = index;
+        destNum = days[index] + 30;
+        while ( j < days.size() && destNum > days[j] ) {
+            j++;
+        }
+
+        int c = costs[2] + dfs(j,days,costs);
+
+        return  memo[index] = min(a,min(b,c));
+    }
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        memo.resize(366,0);
+        int a = dfs(0,days,costs);
+        return a;
+    }
+};
