8eaf89f1a5598e316babd20f06616c0ef20172fd
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2024-12-31 05:21:52 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (72.65%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     vector<int> memo;
+    int durations[3] = {1, 7, 30};
     int dfs(int index, vector<int>& days, vector<int>& costs){
         if (index == days.size()) return 0;
 
         if (memo[index] != 0) return memo[index];
 
-        // 1 day
-        int j = index;
-        int destDay = days[index] + 1;
-        while ( j < days.size() && destDay > days[j] ) {
-            j++;
+        int inter = INT_MAX;
+
+        for (int k = 0 ; k < 3 ; k++){
+            int d = durations[k];
+            int c = costs[k];
+
+            int j = index;
+            int destDay = days[index] + d;
+            while ( j < days.size() && destDay > days[j] ) {
+                j++;
+            }
+
+            inter = min(inter, c + dfs(j,days,costs));
         }
 
-        int a = costs[0] + dfs(j,days,costs);
-
-        // 1 week
-        j = index;
-        destDay = days[index] + 7;
-        while ( j < days.size() && destDay > days[j] ) {
-            j++;
-        }
-
-        int b = costs[1] + dfs(j,days,costs);
-
-        // 1 month
-        j = index;
-        destDay = days[index] + 30;
-        while ( j < days.size() && destDay > days[j] ) {
-            j++;
-        }
-
-        int c = costs[2] + dfs(j,days,costs);
-
-        return  memo[index] = min(a,min(b,c));
+        return  memo[index] = inter;
     }
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
