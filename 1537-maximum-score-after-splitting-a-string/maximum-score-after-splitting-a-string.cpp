e952782b8b7bc7ab69b3c4b6e811370c3c130bbc
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2024-12-31 19:35:47 -0500

    Time: 15 ms (5.34%) | Memory: 9.3 MB (15.33%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int n = s.length();
+        vector<int> backs(n,0);
+
+        backs[n-1] =  (s[n-1] == '1' ? 1 : 0);
+        for (int i = n - 2 ; i >=0 ; i--){
+            backs[i] = backs[i+1] + (s[i] == '1' ? 1: 0);
+        }
+
+        int count = 0;
+        int maxScore = INT_MIN;
+        for (int i = 0 ; i < n - 1; i++){
+            if (s[i] == '0') {
+                count++;
+            }
+            maxScore = max(maxScore, count + (i + 1 < n ? backs[i+1] : 0));
+            cout<<maxScore<<" ";
+        }
+
+        return maxScore;
+        
+    }
+};
