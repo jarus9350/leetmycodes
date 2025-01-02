cbd1b89f1a25c19b2a0bb0c8dd000452055e6f1a
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-02 16:13:22 -0500

    Time: 16 ms (26.24%) | Memory: 35.1 MB (27.37%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int possible(vector<int>& weights, int days, int maxWeight){
+        int totalDays = 1;
+        int sum = 0;
+        for (int i = 0 ; i < weights.size() ; i++){
+            sum += weights[i];
+            if (sum > maxWeight){
+                totalDays++;
+                sum = weights[i];
+            }
+        }
+
+        // if (sum > maxWeight) totalDays++;
+
+        cout<<totalDays<<" "<<maxWeight<<endl;
+
+        return totalDays <= days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {        
+        int totalWeight = 0;
+        int l = INT_MIN;
+        for (auto w: weights)  {
+            totalWeight += w;
+            l = max(l,w);
+        }
+
+        int h = totalWeight;
+        int ans = INT_MAX;
+
+        while (l <= h){
+            int m = l + (h - l)/2;
+            if (possible(weights,days,m)) {
+                // cout<<"possible "<<m<<endl;
+                ans = min(ans,m);
+                h = m-1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return ans;
+    }
+};
