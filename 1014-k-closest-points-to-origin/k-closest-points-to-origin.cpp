87ad8798cfcdce84c72f6161078e23db307fb738
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-02 03:16:24 -0500

    Time: 23 ms (91.93%) | Memory: 64.8 MB (84.37%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,21 +1,19 @@
+struct Compare {
+    bool operator()( const vector<int>& a, const vector<int>& b) {
+        return ( a[0]*a[0] + a[1]*a[1]  < b[0]*b[0] + b[1]*b[1]);
+    }
+};
+
 class Solution {
 public:
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<double,vector<int>>, vector<pair<double,vector<int>>>, greater<pair<double,vector<int>>>> pq;
         vector<vector<int>> ans;
 
-        for (auto p: points){
-            int x = p[0];
-            int y = p[1];
-            double distance = sqrt(x*x + y*y);
-            pq.push({distance,p});
-        }
+        sort(points.begin(),points.end(), Compare());
 
-        while (k--){
-            auto [distance, point] = pq.top();
-            pq.pop();
-
-            ans.push_back(point);
+        for (int i = 0 ; i < k ; i++){
+            ans.push_back(points[i]);
         }
 
         return ans;
