891537b8b991125f320b3feeefc06044b8773cab
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-05 20:44:42 -0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.56%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,31 +3,31 @@ public:
     vector<int> minOperations(string boxes) {
         int l = boxes.size();
         vector<int> ans(l,0);
-        
+
         int prefixO = 0;
         int prefixC = 0;
+        int postfixO = 0;
+        int postfixC = 0;
+
         for (int i = 0 ; i < l ; i++){
             if (i == 0){
                 if (boxes[i] == '1') prefixO = 1;
-                continue;
-            }
-        
-            prefixC = prefixC + prefixO;
-            prefixO = prefixO + (boxes[i] == '1' ? 1 : 0);
-            ans[i] += prefixC;
-        }
-
-        int postfixO = 0;
-        int postfixC = 0;
-        for (int i = l - 1 ; i >= 0 ; i--){
-            if (i == l-1 ){
-                if (boxes[l-1] == '1') postfixO = 1;
-                continue;
+            } else {
+                prefixC = prefixC + prefixO;
+                prefixO = prefixO + (boxes[i] == '1' ? 1 : 0);
+                ans[i] += prefixC;
             }
 
-            postfixC = postfixC + postfixO;
-            postfixO = postfixO + (boxes[i] == '1' ? 1 : 0);
-            ans[i] += postfixC;
+            int j = l - 1 - i;
+
+            if (j == l-1 ){
+                if (boxes[j] == '1') postfixO = 1;
+            } else {
+                postfixC = postfixC + postfixO;
+                postfixO = postfixO + (boxes[j] == '1' ? 1 : 0);
+                ans[j] += postfixC;
+            }
+
         }
         
         return ans;
