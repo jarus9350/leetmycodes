c515cc20c1119ac5eb6b07fb47e047dfb3c90171
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-05 20:41:35 -0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.56%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,20 +3,7 @@ public:
     vector<int> minOperations(string boxes) {
         int l = boxes.size();
         vector<int> ans(l,0);
-
-        vector<int> prefixCost(l,0);
-        vector<int> prefixOnes(l,0);
-
-        vector<int> postfixCost(l,0);
-        vector<int> postfixOnes(l,0);
-
-        // for (int i = 1 ; i < l ; i++){
-        //     prefixCost[i] = ((boxes[i-1] == '1') ? 1 : 0) + prefixCost[i-1];
-        // }
-
-        // for (int i = l - 2; i >= 0  ; i--){
-        //     postfixCost[i] = ((boxes[i+1] == '1') ? 1 : 0) + postfixCost[i+1];
-        // }
+        
         int prefixO = 0;
         int prefixC = 0;
         for (int i = 0 ; i < l ; i++){
@@ -42,36 +29,6 @@ public:
             postfixO = postfixO + (boxes[i] == '1' ? 1 : 0);
             ans[i] += postfixC;
         }
-
-
-
-        // for (auto p : prefixCost){
-        //     cout<<p<<" ";
-        // }
-        // cout<<endl;
-
-        // for (auto p : postfixCost){
-        //     cout<<p<<" ";
-        // }
-        // cout<<endl;
-
-        // for (int i = 0 ; i < l ; i++){
-        //     ans[i] = prefixCost[i] + postfixCost[i];
-        // }
-
-        // for (int i = 0 ; i < l ; i++){
-        //     int count = 0;
-        //     for (int j = 0 ; j < l ; j++){
-        //         if (i != j){
-        //             if (boxes[j] == '1'){
-        //                 count += abs(j-i);
-        //             }
-        //         }
-        //     }
-        //     ans[i] = count;
-        // }
-
-
         
         return ans;
     }
