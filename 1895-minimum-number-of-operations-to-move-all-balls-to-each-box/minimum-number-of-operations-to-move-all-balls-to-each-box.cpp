8f50fd0902dde883ac740a37f385b8c7d43420e4
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-05 20:26:15 -0500

    Time: 1 ms (68.94%) | Memory: 13.2 MB (6.09%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -4,17 +4,67 @@ public:
         int l = boxes.size();
         vector<int> ans(l,0);
 
+        vector<int> prefixCost(l,0);
+        vector<int> prefixOnes(l,0);
+
+        vector<int> postfixCost(l,0);
+        vector<int> postfixOnes(l,0);
+
+        // for (int i = 1 ; i < l ; i++){
+        //     prefixCost[i] = ((boxes[i-1] == '1') ? 1 : 0) + prefixCost[i-1];
+        // }
+
+        // for (int i = l - 2; i >= 0  ; i--){
+        //     postfixCost[i] = ((boxes[i+1] == '1') ? 1 : 0) + postfixCost[i+1];
+        // }
+
         for (int i = 0 ; i < l ; i++){
-            int count = 0;
-            for (int j = 0 ; j < l ; j++){
-                if (i != j){
-                    if (boxes[j] == '1'){
-                        count += abs(j-i);
-                    }
-                }
+            if (i == 0){
+                if (boxes[i] == '1') prefixOnes[i] = 1;
+                continue;
             }
-            ans[i] = count;
+            prefixOnes[i] = prefixOnes[i-1] + (boxes[i] == '1' ? 1 : 0);
+            prefixCost[i] = prefixCost[i-1] + prefixOnes[i-1];
         }
+
+        for (int i = l - 1 ; i >= 0 ; i--){
+            if (i == l-1 ){
+                if (boxes[l-1] == '1') postfixOnes[i] = 1;
+                continue;
+            }
+            postfixOnes[i] = postfixOnes[i+1] + (boxes[i] == '1' ? 1 : 0);
+            postfixCost[i] = postfixCost[i+1] + postfixOnes[i+1];
+        }
+
+
+
+        // for (auto p : prefixCost){
+        //     cout<<p<<" ";
+        // }
+        // cout<<endl;
+
+        // for (auto p : postfixCost){
+        //     cout<<p<<" ";
+        // }
+        // cout<<endl;
+
+        for (int i = 0 ; i < l ; i++){
+            ans[i] = prefixCost[i] + postfixCost[i];
+        }
+
+        // for (int i = 0 ; i < l ; i++){
+        //     int count = 0;
+        //     for (int j = 0 ; j < l ; j++){
+        //         if (i != j){
+        //             if (boxes[j] == '1'){
+        //                 count += abs(j-i);
+        //             }
+        //         }
+        //     }
+        //     ans[i] = count;
+        // }
+
+
         
         return ans;
     }
