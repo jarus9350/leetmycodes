a71da8725ab611cc2fbd4dc9c1a4941da5cbd900
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-10 13:05:27 -0500

    Time: 87 ms (31.51%) | Memory: 129.5 MB (27.40%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<int> freq(26,0);
+
+        for (auto w: words2){
+            vector<int> interFreq(26,0);
+
+            for (auto ch: w){
+                interFreq[ch-'a']++;
+            }
+
+            for (int i = 0 ; i < 26 ; i++){
+                freq[i] = max(freq[i],interFreq[i]);
+            }
+        }
+
+        for (auto c: freq){
+            cout<<c<<" ";
+        }
+        cout<<endl;
+
+        vector<string> ans;
+
+        for (auto w : words1){
+            auto inter = freq;
+
+            vector<int> interFreq(26,0);
+
+            for (auto ch: w){
+                interFreq[ch-'a']++;
+            }
+
+            bool possible = true;
+            for (int i = 0 ; i < 26 ; i++){
+                int a = interFreq[i] - inter[i];
+                if ( a < 0) possible = false;
+            }
+
+            if (possible) ans.push_back(w);
+
+        }
+
+        return ans;
+    }
+};
