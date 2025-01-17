584bcb61c92210a319d51a0a72745a4a9ef107b5
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-17 16:13:56 -0500

    Time: 8 ms (29.75%) | Memory: 263.9 MB (50.63%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -4,20 +4,12 @@ public:
         int size = derived.size();
 
         int rollingStart0 = 0;
-        for (int i = 0 ; i < size ; i++){
-            int d = derived[i];
-            int xorVal = d ^ rollingStart0;
-            rollingStart0 = xorVal;
-        }
+        for (int i = 0 ; i < size ; i++) rollingStart0 = derived[i] ^ rollingStart0;
 
         if (rollingStart0 == 0) return true;
 
         int rollingStart1 = 1;
-        for (int i = 0 ; i < size ; i++){
-            int d = derived[i];
-            int xorVal = d ^ rollingStart1;
-            rollingStart1 = xorVal;
-        }
+        for (int i = 0 ; i < size ; i++) rollingStart1 = derived[i] ^ rollingStart1;
 
         if (rollingStart1 == 1) return true;
 
