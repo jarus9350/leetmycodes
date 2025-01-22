d7fffc08457033c802bd9871e4022252c016c576
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-01-21 20:21:13 -0500

    Time: 2039 ms (5.09%) | Memory: 465.5 MB (5.09%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isValid(int x, int y , int& r, int& c){
+        return (x >=0 && x < r && y >= 0 && y < c);
+    }
+
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int r = isWater.size();
+        int c = isWater[0].size();
+
+        priority_queue<vector<int>,vector<vector<int>>, greater<vector<int>>> pq;
+
+        vector<vector<int>> heights(r,vector<int>(c,0));
+        vector<vector<int>> vis(r,vector<int>(c,0));
+        vector<vector<int>> dir = {{0,1},{1,0},{0,-1}, {-1,0}};
+
+        for (int i = 0 ; i < r ; i++){
+            for (int j = 0 ; j < c ; j++){
+                if (isWater[i][j] == 1) {
+                    pq.push({0,i,j});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        while (!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+
+            int height = top[0];
+            int i = top[1];
+            int j = top[2];
+
+            heights[i][j] = height;
+
+            for (auto d: dir){
+                int x = i + d[0];
+                int y = j + d[1];
+
+                if (isValid(x,y,r,c) && !vis[x][y]){
+                    pq.push({height+1,x,y});
+                    vis[x][y] = 1;
+                }
+            }
+
+
+        }
+
+        return heights;
+        
+    }
+};
