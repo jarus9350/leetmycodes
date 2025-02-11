3b85b08f67606809bb5facd4bcf1716406347a69
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-02-11 00:13:01 -0500

    Time: 7 ms (5.85%) | Memory: 12.3 MB (6.15%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        // reverse(part.begin(),part.end());
+        cout<<part<<endl;
+        string st = "";
+        int partLen = part.length();
+
+        for (auto c : s){
+            st.push_back(c);
+            int l = st.length() - partLen;
+            string inter = (l >= 0) ? st.substr(l) : "";
+            if (inter == part) {
+                int count = partLen;
+                while (count--){
+                    st.pop_back();
+                }
+            }
+        }
+
+        return st;
+        
+    }
+};
