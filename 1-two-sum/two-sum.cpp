6d2e4a03b22aebfdd8bbc26f9942613a76d5395b
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-02-13 15:31:32 -0500

    Time: 0 ms (100.00%) | Memory: 14.7 MB (48.10%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,31 +3,23 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int size = nums.size(); 
         vector<pair<int,int>> nNums;
+
         for (int i = 0 ; i < size; i++){
             nNums.push_back({nums[i],i});
         }
 
         sort(nNums.begin(),nNums.end());
 
-        for (int i = 0 ; i < size ; i++){
-            auto [number, index] = nNums[i];
-            int toFind = target - number;
+        int l = 0;
+        int h = size - 1;
 
-            // cout<<i<<" i "<<toFind<<" tofind"<<endl;
-
-            int l = i + 1;
-            int h = size - 1;
-
-            while (l <= h){
-                int mid = (l + h)/2;
-
-                if (nNums[mid].first < toFind){
-                    l = mid + 1;
-                } else if (nNums[mid].first > toFind){
-                    h = mid - 1;
-                } else if (nNums[mid].first == toFind){
-                    return {index,nNums[mid].second};
-                }
+        while (l < h){
+            if (nNums[l].first + nNums[h].first > target){
+                h--;
+            } else if (nNums[l].first + nNums[h].first < target){
+                l++;
+            } else if (nNums[l].first + nNums[h].first == target){
+                return {nNums[l].second, nNums[h].second};
             }
         }
 
