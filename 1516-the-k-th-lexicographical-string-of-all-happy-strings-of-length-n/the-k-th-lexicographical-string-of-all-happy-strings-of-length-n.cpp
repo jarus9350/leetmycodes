a8e7e9a7ae3ed16e13582ad786efae5438c47e92
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-02-19 18:15:53 -0500

    Time: 632 ms (5.39%) | Memory: 9 MB (79.35%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string ans = "";
+
+    void dfs(string inter,int i,int& n,int& k,int& current){
+        if (current == k && inter.size() == n) {
+            ans = inter;
+            return;
+        }
+
+
+        if (i == n ) {
+            return;
+        } 
+
+    
+        string alphas = "abc";
+
+        for (auto c: alphas){
+            if (inter.empty() || inter.back() != c){
+                inter.push_back(c);
+
+                if (inter.size() == n) {
+                    current++;
+
+                    cout<<inter<<" "<<current<<endl;
+                }
+
+                
+                dfs(inter,i+1,n,k,current);
+
+                inter.pop_back();
+            }
+        }
+
+        return;
+    }
+
+
+    string getHappyString(int n, int k) {
+        string inter = "";
+        int total = 0;
+        dfs(inter,0,n,k,total);
+        
+        return ans;
+    }
+};
