79abc9f58c6b9d0743f002483c5b1e4bdb34b0b8
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-02-20 16:54:24 -0500

    Time: 6 ms (58.06%) | Memory: 21.2 MB (13.18%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool dfs(int i, vector<int>& vis, unordered_map<int,vector<int>>& adjList){
+        if (vis[i]) return false;
+        if (adjList[i].size() == 0) return true;
+
+        vis[i] = 1;
+
+        for (auto pre: adjList[i]){
+            bool ans = dfs(pre,vis,adjList);
+            if (!ans) return false;
+            
+        }
+
+        vis[i] = 0;
+        adjList[i] = vector<int>();
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> adjList;
+        for (int i = 0 ; i < numCourses ; i++) adjList[i] = vector<int>();
+        
+        vector<int> vis(numCourses,0);
+        
+        for (auto a : prerequisites){
+            int to = a[0];
+            int from = a[1];
+            adjList[to].push_back(from);
+        }
+
+        for (int i = 0 ; i < numCourses; i++){
+            if (!vis[i]){
+                bool ans = dfs(i,vis,adjList);
+                if (!ans)  return false;
+            }
+        }
+
+        return true;
+    }
+};
