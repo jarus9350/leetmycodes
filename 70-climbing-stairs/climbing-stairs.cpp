dde90d1aa63ca950f724ac68e9c5b76b495e3435
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-02-27 21:57:21 -0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.57%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,0);
+        if (n >= 1) dp[1] = 1;
+        if (n >= 2) dp[2] = 2;
+
+        if (n == 1 || n == 2) return dp[n];
+
+        for (int i = 3; i <= n ; i++ ){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
+
+
+
+
+
+
+
+
