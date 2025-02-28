b5473f4eba08f7f27278b4812f708a0bbc822dff
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-02-27 23:10:27 -0500

    Time: 19 ms (8.49%) | Memory: 39.2 MB (52.15%) - LeetSync

---
+++ 359-logger-rate-limiter/logger-rate-limiter.cpp
@@ -0,0 +1,25 @@
+class Logger {
+public:
+    unordered_map<string,int> mp;
+    Logger() {
+        
+    }
+    
+    bool shouldPrintMessage(int timestamp, string message) {
+        if (mp.find(message) != mp.end() && timestamp - mp[message] < 10) {
+            return false;
+        }
+
+        mp[message] = timestamp;
+        return true;
+        
+
+
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
