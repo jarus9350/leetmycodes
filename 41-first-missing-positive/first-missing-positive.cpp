19e4d82ac6a445e7a8ead9ecdc08fad3cb3d0800
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-03-02 21:54:18 -0500

    Time: 762 ms (5.14%) | Memory: 54.9 MB (93.33%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        // all negatives to zero
+        for (int i = 0 ; i < n ; i++) { 
+            if ( nums[i] < 0 ){
+                nums[i] = 0;
+            }
+        }
+
+        int defaultValue = -(n + 1);
+
+        for (int i = 0 ; i < n ; i++) {
+            int num = nums[i];
+            int index = abs(num) - 1;
+
+            if (index >= 0 && index < n) {
+                int numAtIndex = nums[index];
+
+                if (numAtIndex == 0) {
+                    nums[index] = defaultValue;
+                } else {
+                    if (numAtIndex < 0) {
+                        continue;
+                    } else {
+                        nums[index] = -nums[index];
+                    }
+                }
+            }            
+        }
+
+        for (auto n: nums){
+            cout<<n<<" ";
+        }
+        cout<<endl;
+
+        for (int i = 1 ; i <= n ; i++) {
+            if (nums[i-1] < 0 || nums[i-1] == defaultValue)  {
+                continue;
+            } else {
+                return i;
+            }
+        }
+
+
+        return n+1;
+
+        
+    }
+};
