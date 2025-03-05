777924322de74a7e725cbd12b317505f8193d0f8
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-03-05 02:06:20 -0500

    Time: 248 ms (38.92%) | Memory: 185.6 MB (36.21%) - LeetSync

--- 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.cpp
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.cpp
@@ -2,25 +2,33 @@ class Solution {
 public:
     int getLargestOutlier(vector<int>& nums) {
         int n = nums.size();
-        map<double,int> mp;
-        double totalSum = 0;
+        unordered_map<double,int> mp;
+        double total = 0;
 
-        for (int i = 0 ; i < n ; i++){
-            int n = nums[i];
-            totalSum+= n;
-            mp[n] = i;
+        for (auto n: nums){
+            total += n;
+            mp[(double)n]++;
         }
 
         int ans = INT_MIN;
+
         for (int i = 0 ; i < n ; i++){
-            int n = nums[i];
-            double m = ((double)(totalSum - n))/2;
-            if (mp.find(m) != mp.end() && i != mp[m]) {
-                // cout<<m<<" "<<n<<endl;
-                ans = max(ans,n);
+            int num = nums[i];
+            double remaining = (double)(total - num)/2;
+
+            if (mp.find(remaining) != mp.end()) {
+                mp[num]--; //considering
+
+                int occ = mp[remaining];
+                if (occ > 0) ans = max(ans,num);
+
+                mp[num]++; //adding back
             }
         }
-       
+
         return ans;
+
+
+        
     }
 };
