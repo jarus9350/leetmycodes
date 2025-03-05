d105fac13118979849bb29734d97c51cc7de91e8
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-03-05 00:41:24 -0500

    Time: 2 ms (55.29%) | Memory: 12 MB (33.07%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -4,19 +4,23 @@ public:
         string st = "";
         int partLen = part.length();
 
-        for (auto c : s){
+        for (auto c: s){
             st.push_back(c);
-            int l = st.length() - partLen;
-            string inter = (l >= 0) ? st.substr(l) : "";
+            int currLen = st.size();
+            int index = currLen - partLen;
+            
+            string inter = (index >= 0) ? st.substr(index) : "";
+
             if (inter == part) {
-                int count = partLen;
-                while (count--){
-                    st.pop_back();
-                }
+                int temp = partLen;
+                while(temp--) st.pop_back();
             }
         }
 
         return st;
         
     }
+
+    // 01234
+    // suraj
 };
