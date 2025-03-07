e0f0ea000036d10d610e5640c9689266072aa953
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-03-07 14:08:07 -0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (70.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -2,30 +2,36 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m - 1;
-        int j = n - 1 ;
+        int j = n - 1;
         int k = m + n - 1;
 
-        while (i >= 0 && j >= 0){
-            if (nums1[i] > nums2[j]){
-                nums1[k--] = nums1[i--];
+        while ( i >= 0 || j >= 0) {
+            int a = (i >= 0) ? nums1[i] : INT_MIN;
+            int b = (j >= 0) ? nums2[j] : INT_MIN;
+
+            if (a <= b) {
+                nums1[k--] = b;
+                j--;
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[k--] = a;
+                i--;
             }
+            // if ( nums1[i] <= nums2[j]) {
+            //     nums1[k--] = nums2[j--];
+            // } else {
+            //     nums1[k--] = nums1[i--];
+            // }
         }
 
-        while (i >= 0){
-            nums1[k--] = nums1[i--];
-        }
+        // while ( i >= 0 ){
+        //     nums1[k--] = nums1[i--]; 
+        // } 
+        
 
-        while (j >= 0){
-            nums1[k--] = nums2[j--];
-        }
+        // while ( j >= 0 ){
+        //     nums1[k--] = nums2[j--]; 
+        // } 
 
         return;
-
-        //  1 2 3 0 0 0
-        // 2 5 6
-        //      2 3 5 6
-        
     }
 };
