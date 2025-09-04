3e8c3f3be950f1f4a3952b109b0a730d9d0b932a
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-09-04 19:53:03 -0400

    Time: 0 ms (100.00%) | Memory: 8.4 MB (88.77%) - LeetSync

--- 3830-find-closest-person/find-closest-person.cpp
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        int dist1 = x - z;
-        int dist2 = y - z;
-
-        if (dist1 < 0) {
-            dist1 *= -1;
-        }
-
-        if (dist2 < 0){
-            dist2 *= -1;
-        }
+        int dist1 = abs(x - z);
+        int dist2 = abs(y - z);
 
         if (dist1 < dist2) return 1;
         else if (dist2 < dist1) return 2;
