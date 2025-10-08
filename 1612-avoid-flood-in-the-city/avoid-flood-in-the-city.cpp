d897c2a3d51167bc3abf447d09109203e90437d5
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-10-08 16:42:12 -0400

    Time: 107 ms (81.55%) | Memory: 114.7 MB (28.32%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    vector<int> avoidFlood(vector<int>& rains) {
-        vector<int> ans(rains.size(), 1);
-        set<int> st;
-        unordered_map<int, int> mp;
-        for (int i = 0; i < rains.size(); ++i) {
-            if (rains[i] == 0) {
-                st.insert(i);
-            } else {
-                ans[i] = -1;
-                if (mp.count(rains[i])) {
-                    auto it = st.lower_bound(mp[rains[i]]);
-                    if (it == st.end()) {
-                        return {};
-                    }
-                    ans[*it] = rains[i];
-                    st.erase(it);
-                }
-                mp[rains[i]] = i;
+    int binarySearch(set<int>& zeroes, int lake){
+        for (auto ele: zeroes) {
+            if (ele > lake) {
+                int index = ele;
+                zeroes.erase(ele);
+                return ele;
             }
         }
+
+        return -1;
+    }
+    vector<int> avoidFlood(vector<int>& rains) {
+        unordered_map<int,int> lakeRained;
+        vector<int> ans(rains.size(),1);
+        set<int> zeroes;
+
+        for (int i = 0 ; i < rains.size() ; i++){
+            int lake = rains[i];
+            if (lake == 0) {
+                zeroes.insert(i);
+            } else {
+                if (lakeRained.find(lake) == lakeRained.end()) {
+                    lakeRained[lake] = i;
+                    ans[i] = -1;
+                } else {
+                    int lastSeen = lakeRained[lake];
+                    lakeRained[lake] = i;
+                    int index = binarySearch(zeroes, lastSeen);
+                    if (index == -1) return {};
+                    ans[index] = lake;
+                    ans[i] = -1;
+                }
+            }
+        }
+
         return ans;
+        
     }
 };
