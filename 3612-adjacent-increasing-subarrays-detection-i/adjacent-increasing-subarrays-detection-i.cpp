47015ff529d0116352da0b00aa5b92526fd6d2f9
suraj pratap singh <52481619+jarus9350@users.noreply.github.com>
2025-10-14 17:12:01 -0400

    Time: 39 ms (5.45%) | Memory: 42.1 MB (10.00%) - LeetSync

---
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        if (k == 1 ) return true;
+
+        int n = nums.size();
+        vector<int> tillI(n,1);
+        vector<int> fromI(n,1);
+
+        for (int i = 0 ; i < n ; i++){
+            if ( i - 1 >= 0 && nums[i] > nums[i-1] ) tillI[i] += tillI[i-1];
+        }
+
+        for (int i = n - 1 ; i>= 0 ; i--){
+            if ( i + 1 < n && nums[i] < nums[i+1]) fromI[i] += fromI[i+1];
+        }
+
+        for (auto c: tillI){
+            cout<<c<<" ";
+        }
+        cout<<endl;
+
+        for (auto c: fromI){
+            cout<<c<<" ";
+        }
+        cout<<endl;
+
+        for (int i = 0 ; i < n ; i++){
+            if (  i+ 1 < n && tillI[i] >= k && fromI[i+1] >=k ) return true;
+        }
+
+        return 0;
+        
+    }
+};
